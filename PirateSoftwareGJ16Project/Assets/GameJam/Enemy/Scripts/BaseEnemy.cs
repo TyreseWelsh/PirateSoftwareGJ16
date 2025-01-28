2de40a800341dd1c2e48b657fe17b2bcc0223200
Jack Burrup <62547604+Quertxx@users.noreply.github.com>
2025-01-28 14:54:54 +0000

    AI follows player after spawning in.

--- PirateSoftwareGJ16Project/Assets/GameJam/Enemy/Scripts/BaseEnemy.cs
+++ PirateSoftwareGJ16Project/Assets/GameJam/Enemy/Scripts/BaseEnemy.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour
 {
+
+    private NavMeshAgent agent;
+
+    [SerializeField]private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) > 2.0f)
+        {
+            Debug.Log("Following");
+            agent.destination = player.transform.position;
+        }
     }
 }
